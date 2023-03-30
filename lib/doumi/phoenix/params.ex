6b0e4df4ea6192fce54e5cb53581e6c83681ea85
Jinkyou Son <nallwhy@gmail.com>
2023-03-30 16:02:17 +0900

    Implement Doumi.Phoenix.Params.{to_params/2, to_form/3}

--- lib/doumi/phoenix/params.ex
+++ lib/doumi/phoenix/params.ex
@@ -1,18 +1,36 @@
 defmodule Doumi.Phoenix.Params do
-  @moduledoc """
-  Documentation for `Doumi.Phoenix.Params`.
-  """
+  def to_params(form_or_changeset, more_params \\ %{})
 
-  @doc """
-  Hello world.
+  def to_params(%Phoenix.HTML.Form{source: %Ecto.Changeset{} = changeset}, more_params) do
+    to_params(changeset, more_params)
+  end
 
-  ## Examples
+  def to_params(%Ecto.Changeset{} = changeset, more_params) do
+    changeset.params
+    |> Map.merge(more_params)
+  end
 
-      iex> Doumi.Phoenix.Params.hello()
-      :world
+  def to_form(params, module_or_changeset_fun, opts \\ [])
 
-  """
-  def hello do
-    :world
+  def to_form(params, module, opts) when is_atom(module) do
+    to_form(params, &module.changeset/1, opts)
+  end
+
+  def to_form(params, changeset_fun, opts) when is_function(changeset_fun, 1) do
+    {validate, opts} = opts |> Keyword.pop(:validate, true)
+
+    params
+    |> changeset_fun.()
+    |> do_validate(validate)
+    |> Phoenix.Component.to_form(opts)
+  end
+
+  defp do_validate(%Ecto.Changeset{} = changeset, true) do
+    changeset
+    |> Map.put(:action, :validate)
+  end
+
+  defp do_validate(%Ecto.Changeset{} = changeset, false) do
+    changeset
   end
 end
--- test/doumi/phoenix/params_test.exs
+++ test/doumi/phoenix/params_test.exs
@@ -1,8 +1,83 @@
 defmodule Doumi.Phoenix.ParamsTest do
   use ExUnit.Case
-  doctest Doumi.Phoenix.Params
+  alias Doumi.Phoenix.Params
 
-  test "greets the world" do
-    assert Doumi.Phoenix.Params.hello() == :world
+  defmodule Test do
+    use Ecto.Schema
+    import Ecto.Changeset
+
+    embedded_schema do
+      field(:foo, :string)
+    end
+
+    def changeset(params) do
+      %__MODULE__{}
+      |> cast(params, [:foo])
+      |> validate_required([:foo])
+    end
+  end
+
+  describe "to_params/2" do
+    test "returns params from changeset" do
+      changeset = Test.changeset(%{foo: "bar"})
+
+      assert Params.to_params(changeset) == %{"foo" => "bar"}
+    end
+
+    test "returns params from form" do
+      form = Test.changeset(%{foo: "bar"}) |> Phoenix.Component.to_form()
+
+      assert Params.to_params(form) == %{"foo" => "bar"}
+    end
+
+    test "returns params merged with more_params" do
+      changeset = Test.changeset(%{foo: "bar"})
+
+      assert Params.to_params(changeset, %{"bar" => "baz"}) == %{"foo" => "bar", "bar" => "baz"}
+    end
+  end
+
+  describe "to_form/3" do
+    test "returns form from params and changeset_fun" do
+      params = %{"foo" => "bar"}
+
+      assert %Phoenix.HTML.Form{source: %Ecto.Changeset{} = source} =
+               Params.to_form(params, &Test.changeset/1)
+
+      assert source.action == :validate
+      assert source.changes == %{foo: "bar"}
+    end
+
+    test "returns form from params and module" do
+      params = %{"foo" => "bar"}
+
+      assert %Phoenix.HTML.Form{source: %Ecto.Changeset{} = source} = Params.to_form(params, Test)
+      assert source.action == :validate
+      assert source.changes == %{foo: "bar"}
+    end
+
+    test "returns form from params and module with validate: false" do
+      params = %{"foo" => "bar"}
+
+      assert %Phoenix.HTML.Form{source: %Ecto.Changeset{} = source} =
+               Params.to_form(params, Test, validate: false)
+
+      assert source.action == nil
+      assert source.changes == %{foo: "bar"}
+    end
+
+    test "returns form from params and module with opts" do
+      params = %{"foo" => "bar"}
+
+      assert %Phoenix.HTML.Form{source: %Ecto.Changeset{} = source} =
+               form = Params.to_form(params, Test, id: :test_id, name: :test_name, as: :test_as)
+
+      assert source.action == :validate
+      assert source.changes == %{foo: "bar"}
+      assert form.id == :test_id
+      assert form.name == "test_as"
+      assert form.options |> Keyword.get(:id) == :test_id
+      assert form.options |> Keyword.get(:name) == :test_name
+    end
   end
 end
