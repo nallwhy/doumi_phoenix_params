965162e923f580ed298309f7e3ed76155df4aa86
Jinkyou Son <nallwhy@gmail.com>
2023-03-30 15:10:53 +0900

    Init project

---
+++ lib/doumi/phoenix/params.ex
@@ -0,0 +1,18 @@
+defmodule Doumi.Phoenix.Params do
+  @moduledoc """
+  Documentation for `Doumi.Phoenix.Params`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Doumi.Phoenix.Params.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/doumi/phoenix/params_test.exs
@@ -0,0 +1,8 @@
+defmodule Doumi.Phoenix.ParamsTest do
+  use ExUnit.Case
+  doctest Doumi.Phoenix.Params
+
+  test "greets the world" do
+    assert Doumi.Phoenix.Params.hello() == :world
+  end
+end
